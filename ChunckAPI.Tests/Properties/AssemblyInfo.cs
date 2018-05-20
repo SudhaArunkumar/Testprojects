522dc8b46c2f68ad36e4a02739ac66d27d23850c
Sudha Arunkumar <sstsudha@gmail.com>
2018-05-20 16:48:45 +0530

    updated code

---
+++ ChunckAPI.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ChunckAPI.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ChunckAPI.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("aec73880-5afa-4765-8afb-b550a5315b41")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ChunckAPI/Controllers/ValuesController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using Newtonsoft.Json;
+
+using System.Web.Http;
+
+namespace ChunckAPI.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+       // public string Get(string id)
+        public string Post([FromBody] dynamic jsonstring)
+        {
+            int partLength = 25;
+            //string id1 =
+            //   "Please write a program that breaks this text into small chucks. Each chunk should have a maximum length of 25 " +
+            //   "characters. The program should try to break on complete sentences or punctuation marks if possible.  " +
+            //   "If a comma or sentence break occurs within 5 characters of the max line length, the line should be broken early.  " +
+            //   "The exception to this rule is when the next line will only contain 5 characters.  Redundant whitespace should " +
+            //   "not be counted between lines, and any duplicate   spaces should be removed.  " +
+            //   "Does this make sense? If not please ask for further clarification, an array containing " +
+            //   "the desired outcome has been provided below. Any text beyond this point is not part of the instructions, " +
+            //   "it's only here to ensure test converge. Finish line. Aaa asdf asdfjk las, asa.";
+            string convert = jsonstring;
+            string[] words = convert.Split(' ');
+            List<string> list = new List<string>(words);
+            //List<APIOutput> outputlist = new List<APIOutput>();
+
+            List<string> outputlist = new List<string>();
+            StringBuilder builder = new StringBuilder();
+            string part = string.Empty;
+
+
+           // string part = string.Empty;
+
+            bool sentencebreak = false;
+            string tocheck = string.Empty;
+            int length = 0;
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (part.Length + list[i].Length <= partLength)
+                {
+                    if (sentencebreak)
+                    {
+                        part = string.Empty;
+                        sentencebreak = false;
+                    }
+                    part += string.IsNullOrEmpty(part) ? list[i] : " " + list[i];
+                    tocheck = list[i];
+                    list[i] = string.Empty;
+                    if (tocheck.Contains(",") || tocheck.Contains("."))
+                    {
+                        var poscomma = part.IndexOf(',');
+                        var posstop = part.IndexOf('.');
+                        if (poscomma >= 21 || posstop >= 21)
+                        {
+                            foreach (string w in list)
+                            {
+                                if (!string.IsNullOrEmpty(w))
+                                {
+                                    length += w.Length;
+                                }
+                            }
+                            if (length > 5)
+                            {
+
+
+                                builder.Append(part).AppendLine();
+                                //outputlist.Add(new APIOutput { chunckoutput = part });
+                                sentencebreak = true;
+                                part = string.Empty;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+
+                    // outputlist.Add(new APIOutput { chunckoutput = part });
+                    builder.Append(part).AppendLine();
+                    part = list[i];
+
+                }
+
+            }
+
+            // outputlist.Add(new APIOutput { chunckoutput = part });
+            builder.Append(part).AppendLine();
+            string output = builder.ToString();
+            return output;
+            
+
+        }
+
+        //public List<string> Get(string para)
+        //{
+        //    List<string> outputlist = new List<string>();
+        //    return outputlist;
+        //}
+
+        // POST api/values
+       // public String Post([FromBody]string value)
+      //  {
+
+       //     return "Sucess";
+       // }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+
+    public class APIOutput
+    {
+        public string chunckoutput { get; set; }
+
+    }
+}
